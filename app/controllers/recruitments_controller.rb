58cb8d6c7c6a053c4e5a54d999234608bb02ba99
Muran94 <headiv94@gmail.com>
2018-03-05 08:36:41 +0900

    flashメッセージを表示できるようにした  - RecruitmentsControllerのcreate, update, destroyアクションにflashメッセージを仕込むための処理を追加  - _flash.html.slimパーシャルの作成と適用

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -15,8 +15,10 @@ class RecruitmentsController < ApplicationController
   def create
     @recruitment = current_user.recruitments.build(_recruitment_params)
     if @recruitment.save
+      flash[:success] = "募集記事の作成が完了しました。"
       redirect_to @recruitment
     else
+      flash[:success] = "募集記事の作成に失敗しました。"
       render :new
     end
   end
@@ -26,13 +28,16 @@ class RecruitmentsController < ApplicationController
 
   def update
     if @recruitment.update(_recruitment_params)
+      flash[:success] = "募集記事の更新に完了しました。"
       redirect_to @recruitment
     else
+      flash[:success] = "募集記事の更新に失敗しました。"
       render :edit
     end
   end
 
   def destroy
+    flash[:success] = "募集記事の削除が完了しました。"
     @recruitment.delete
     redirect_to root_path
   end
