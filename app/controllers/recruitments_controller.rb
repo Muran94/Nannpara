e297a4d97548137ec77f84627f36772a8247d6ec
Muran94 <headiv94@gmail.com>
2018-03-05 07:25:12 +0900

    UserモデルとRecruitmentモデルの関連付け RecruitmentsControllerのindex, show, new, create周りの基本的な実装 Recruitment用のformパーシャルの作成と適用

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,14 +1,24 @@
 class RecruitmentsController < ApplicationController
+  before_action :_get_recruitment, only: [:show, :edit, :update, :destroy]
+
   def index
+    @recruitments = Recruitment.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @recruitment = Recruitment.new
   end
 
   def create
+    @recruitment = current_user.recruitments.build(_recruitment_params)
+    if @recruitment.save
+      redirect_to @recruitment
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +28,20 @@ class RecruitmentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def _get_recruitment
+    @recruitment = Recruitment.find(params[:id])
+  end
+
+  def _recruitment_params
+    params.require(:recruitment).permit(
+      :title,
+      :description,
+      :event_date,
+      :venue
+    )
   end
 end
