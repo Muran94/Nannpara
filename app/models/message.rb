27f7da9843e8021b4b82a07f3ee5df10b6f3d81b
Muran94 <headiv94@gmail.com>
2018-03-05 19:59:13 +0900

    Messageモデルの作成

---
+++ app/models/message.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id             :integer          not null, primary key
+#  message        :text
+#  user_id        :integer
+#  recruitment_id :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :recruitment
+end
