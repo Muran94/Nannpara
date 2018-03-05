f34e3401d523e9a401359b689e946ee819c495d8
Muran94 <headiv94@gmail.com>
2018-03-05 19:54:50 +0900

    annotate実行

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: recruitments
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  event_date  :datetime
+#  venue       :string
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Recruitment < ApplicationRecord
   belongs_to :user
 
