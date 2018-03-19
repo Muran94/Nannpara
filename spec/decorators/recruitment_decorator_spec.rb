80286bdf87d8a638e6439d0a241b69bde274c078
Muran94 <headiv94@gmail.com>
2018-03-19 19:06:19 +0900

    不要なdecorator_specを削除

--- spec/decorators/recruitment_decorator_spec.rb
+++ spec/decorators/recruitment_decorator_spec.rb
@@ -11,18 +11,4 @@ describe RecruitmentDecorator do
       end
     end
   end
-
-  describe %(#thumb_image_url) do
-    let(:user) { create(:user) }
-    let(:recruitment) { create(:recruitment, user: user).extend RecruitmentDecorator }
-
-    it %(userが画像を持っていれば その画像URL を、持っていなければ "no_user_image.png" を返す) do
-      aggregate_failures do
-        expect(recruitment.users_thumb_image_url).to eq recruitment.user.image.thumb.url
-        user.image = nil
-        user.save
-        expect(recruitment.users_thumb_image_url).to eq 'no_user_image.png'
-      end
-    end
-  end
 end
