bb310ceba82b42a0db17bd19e4270fc0643722cc
Muran94 <headiv94@gmail.com>
2018-04-02 19:22:44 +0900

    障害対策

--- app/jobs/recruitment_linkage_job.rb
+++ app/jobs/recruitment_linkage_job.rb
@@ -14,17 +14,18 @@ class RecruitmentLinkageJob < ApplicationJob
   def perform(recruitment)
     if recruitment.linked_with_kanto_nanpa_messageboard
       chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-
-      chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+      base_args = %w{headless no-sandbox disable-gpu}
+      chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin, 'args' => base_args } } : {}
 
       Capybara.register_driver :chrome do |app|
         Capybara::Selenium::Driver.new(
-           app,
-           browser: :chrome,
-           desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+          app,
+          browser: :chrome,
+          desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
         )
       end
 
+      Capybara.default_driver = :chrome
       Capybara.default_selector = :xpath
       session = Capybara::Session.new(:selenium_chrome_headless)
       session.visit KANTO_NANPA_MESSAGEBOARD_URL
