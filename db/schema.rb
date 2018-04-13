88b52bc79d547cc468685b74f5d7a89ca587c373
Muran94 <headiv94@gmail.com>
2018-04-13 20:32:35 +0900

    migrationファイルの順番を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411120044) do
+ActiveRecord::Schema.define(version: 20180411110002) do
 
   create_table "activities", force: :cascade do |t|
     t.integer "activity_type_id"
