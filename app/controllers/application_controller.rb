8a4aa6dab859f4acea0087b2708fa4873e5513b6
Muran94 <headiv94@gmail.com>
2018-04-15 00:16:25 +0900

    ステージングにベーシック認証をかける

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :_basic_auth if Rails.env.staging?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :direct_mail])
+  end
+
+  private
+
+  def _basic_auth
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == Settings.security.staging_basic_auth.user_name && password == Settings.security.staging_basic_auth.password
+    end
   end
 end
