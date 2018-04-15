527f39ff778b9477a9a7177dc201f2ca9046d7f9
Muran94 <headiv94@gmail.com>
2018-04-15 14:17:21 +0900

    フッターのコピーライトのview周りのリファクタリング

--- app/helpers/service_helper.rb
+++ app/helpers/service_helper.rb
@@ -1,2 +1,5 @@
 module ServiceHelper
+    def copyright_year
+        Time.zone.now.year == Settings.service.launched_year ? Settings.service.launched_year : %(#{Settings.service.launched_year} - #{Time.zone.now.year})
+    end
 end
